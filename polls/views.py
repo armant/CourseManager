e836c7c4aba006525a8d4799adc00eeede499696
Arman Tokanov <armatoka@gmail.com>
2014-05-16 00:57:50 -0400

    Can display the list of courses and a page for each course

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
 from polls.models import Choice
 # Create your views here.
 
 def index(request):
-	output = '\n'.join([p.choice_text for p in Choice.objects.all()])
-	return HttpResponse(output)
+	course_list = Choice.objects.order_by('choice_text')
+	#template = loader.get_template('polls/index.html')
+	context = {'course_list': course_list}
+	return render(request, 'polls/index.html', context)
 
 def students_list(request, choice_id):
-	return HttpResponse("You're looking at the list of students enrolled in %s." % choice_id)
+	course = get_object_or_404(Choice, pk=choice_id)
+	return render(request, 'polls/course_enrolled.html', {'course': course})
