687e651304ec55efe6c140cf6cf2b1503049637d
Arman Tokanov <armatoka@gmail.com>
2014-05-15 22:01:27 -0400

    Added ability to display courses

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from polls import views
+
+urlpatterns = patterns ('',
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<choice_id>\d+)/$', views.students_list, name='students_list'),
+)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from polls.models import Choice
 # Create your views here.
+
+def index(request):
+	output = '\n'.join([p.choice_text for p in Choice.objects.all()])
+	return HttpResponse(output)
+
+def students_list(request, choice_id):
+	return HttpResponse("You're looking at the list of students enrolled in %s." % choice_id)
