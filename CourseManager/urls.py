ca748863fd408cd50e6fda919c1472a06e72ea4c
Arman Tokanov <armatoka@gmail.com>
2014-05-20 17:03:25 -0400

    Added re-direct from home page to login page

--- CourseManager/urls.py
+++ CourseManager/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^courses/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', name="my_login")
+    url(r'^login/$', 'django.contrib.auth.views.login', name="my_login"),
+    url(r'$', 'django.contrib.auth.views.login', name="my_login"),
 )
